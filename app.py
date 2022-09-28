74ce81301e8b6a2cf0fdade77aa894a6e3955c6d
stephen-w-choo <stephen.w.choo@gmail.com>
2022-09-28 10:19:49 +1000

    crossword route is correctly returning the 2D array - now I need to fix a way to request specific crossword structures

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from flask import Flask, render_template, request
 from static.crossword.generate import generate_crossword
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return generate_crossword()
+
+
+
+
+@app.route("/generate")
+    return generate_crossword(
+        os.path.join("static", "crossword", "data", "structure0.txt"),
+        os.path.join("static", "crossword", "data", "words0.txt") 
+        )
 
